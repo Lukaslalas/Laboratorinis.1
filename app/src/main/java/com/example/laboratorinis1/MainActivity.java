824b4005de39f6c753c08b4be4a9a550c7dd0402
Lukaslalas <lukaslalas10@gmail.com>
2022-03-05 15:06:59 +0200

    8 užduotis

--- app/src/main/java/com/example/laboratorinis1/MainActivity.java
+++ app/src/main/java/com/example/laboratorinis1/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.laboratorinis1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain=(TextView)findViewById(R.id.tvMain); //this kviecia globalu
+        this.tvMain.setText("Pagrindinis langas - Labas");
+    }
+
+    public void onBtnMainClick(View view) {
+        this.tvMain.setText("Paspausta");
+
     }
 }
